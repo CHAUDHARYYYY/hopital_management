67b19c19694042eb3b9aa924cefb6e08c94d91fe
Saikat Mandal <msaikat022@gmail.com>
2023-03-15 21:30:09 +0530

    added jwt

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ import express from "express";
 import mongoose from "mongoose";
 import path from "path";
 import url from "url";
+import jwt from "jsonwebtoken";
 import bodyParser from "body-parser";
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 import cookieParser from "cookie-parser";
 import User from "./models/UserModel.js";
 import authRoutes from "./routes/auth.js";
-import e from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
 app.use(cookieParser());
 app.set("view engine", "ejs");
@@ -16,16 +19,23 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/auth", authRoutes);
+
 // database connection
 mongoose
   .connect("mongodb://0.0.0.0:27017/epicsDb")
   .then(() => console.log("success connecting to db"));
 //////////////////////////middleware////////////////////////
 const checkLoggedIn = (req, res, next) => {
-  if (req.cookies.isLoggedIn) {
-    next();
+  if (req.cookies.Login) {
+    let isVerified = jwt.verify(req.cookies.Login, process.env.JWT_SECRET);
+    if (isVerified) {
+      next();
+    } else {
+      console.log("invalid user");
+    }
   } else {
     console.log("operation not allowed");
+    res.redirect("/signup");
   }
 };
 // get routes
@@ -61,7 +71,7 @@ app.post("/signup", (req, res) => {
       .save()
       .then(() => console.log("user from frontend saved succesfully"));
 
-    res.cookie("isLoggedIn", true, { maxAge: 60 * 1000, httpOnly: true });
+    res.cookie("isLoggedIn", token, { maxAge: 60 * 1000, httpOnly: true });
     res.redirect("/telemedicine");
   } catch (err) {
     console.log(err);
@@ -80,7 +90,16 @@ app.post("/login", async (req, res) => {
 
     if (user) {
       if (user.password === data.password) {
-        res.cookie("isLoggedIn", true, { maxAge: 60 * 1000, httpOnly: true });
+        // payload
+        let payload = user["_id"];
+        // signature
+        let token = jwt.sign(
+          {
+            payload: payload,
+          },
+          process.env.JWT_SECRET
+        );
+        res.cookie("Login", token, { maxAge: 60 * 1000, httpOnly: true });
         res.redirect("/telemedicine");
       } else {
         console.log("wrong credentials");
